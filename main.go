3ebf9525069cf0c139488cf09a202194f4361539
tandy9527 <tengyi555888@gmail.com>
2025-10-11 11:48:33 +0800

    1111

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
-	"js-util/kafka"
 	"time"
+
+	"github.com/tandy9527/js-util/kafka"
 )
 
 func main() {
@@ -41,44 +42,5 @@ func main() {
 		dlqTopic,    // DLQ topic
 	)
 	select {}
-	// consumer := kafka.GetConsumer()
-	// producer := kafka.GetProducer()
 
-	// // 3️⃣ Producer 循环发送消息
-	// go func() {
-	// 	i := 0
-	// 	for {
-	// 		key := []byte(fmt.Sprintf("key-%d", i%3))
-	// 		value := []byte(fmt.Sprintf("msg-%d %s", i, time.Now().Format(time.RFC3339)))
-	// 		if err := producer.SendMessage(key, value); err != nil {
-	// 			log.Printf("[Producer] send failed: %v", err)
-	// 		} else {
-	// 			log.Printf("[Producer] sent: %s", string(value))
-	// 		}
-	// 		i++
-	// 		time.Sleep(time.Second)
-	// 	}
-	// }()
-
-	// // 4️⃣ 等待退出信号
-	// sigs := make(chan os.Signal, 1)
-	// signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	// <-sigs
-
-	// log.Println("Shutting down...")
-	// consumer.Stop()
-	// producer.Close()
-	// log.Println("Exit")
-	// 3️⃣ 初始化 ProducerRunner
-	// kafka.InitConsumer(time.Second, func(i int) ([]byte, []byte) {
-	// 	key := []byte(fmt.Sprintf("key-%d", i%3))
-	// 	value := []byte(fmt.Sprintf("msg-%d %s", i, time.Now().Format(time.RFC3339)))
-	// 	return key, value
-	// })
-
-	// runner := kafka.GetConsumer()
-	// runner.Start()
-
-	// // 阻塞等待退出
-	// runner.WaitForSignal()
 }
