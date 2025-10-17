8081b8197624b12a1cac564eb317ebae6ebd1359
tandy9527 <tengyi555888@gmail.com>
2025-10-17 11:59:02 +0800

    Loadyaml

---
+++ utils/yaml.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"os"
+
+	"github.com/tandy9527/js-util/logger"
+	"gopkg.in/yaml.v3"
+)
+
+// Loadyaml 加载.yaml
+func Loadyaml[T any](filePath string) *T {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		panic("failed to read config file: " + filePath)
+	}
+	var config T
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		panic("failed to unmarshal config file: " + filePath)
+	}
+
+	logger.Infof("[yamlutil] load successful: %s", filePath)
+	return &config
+}
