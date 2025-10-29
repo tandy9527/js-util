764c0ca4dfb1fe29f2584d57b50ef93f66291bec
tandy9527 <tengyi555888@gmail.com>
2025-10-29 14:52:01 +0800

    111

---
+++ cache/const.go
@@ -0,0 +1,20 @@
+package cache
+
+const (
+	DB0  = "db0"
+	DB1  = "db1"
+	DB2  = "db2"
+	DB3  = "db3"
+	DB4  = "db4"
+	DB5  = "db5"
+	DB6  = "db6"
+	DB7  = "db7"
+	DB8  = "db8"
+	DB9  = "db9"
+	DB10 = "db10"
+	DB11 = "db11"
+	DB12 = "db12"
+	DB13 = "db13"
+	DB14 = "db14"
+	DB15 = "db15"
+)
